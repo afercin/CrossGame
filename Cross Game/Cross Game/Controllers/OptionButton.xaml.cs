a2ce71901c2f1a7f35ebe88c64877d2609417f5c
afercin <afercin@gmail.com>
2021-05-08 09:49:42 +0200

    Improve animation

--- Cross Game/Cross Game/Controllers/OptionButton.xaml.cs
+++ Cross Game/Cross Game/Controllers/OptionButton.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media.Animation;
 
 namespace Cross_Game.Controllers
 {
@@ -18,7 +19,7 @@ namespace Cross_Game.Controllers
             set
             {
                 SetValue(ActivePropertyChanged, value);
-                BackColor.Opacity = Active ? 0.75 : 0.0;
+                BackColor.BeginAnimation(OpacityProperty, Active ? ActiveAnimation : InactiveAnimation);
             }
         }
 
@@ -31,31 +32,34 @@ namespace Cross_Game.Controllers
         public static readonly DependencyProperty ActivePropertyChanged = DependencyProperty.Register("Active", typeof(bool), typeof(OptionButton), new PropertyMetadata(false));
         public static readonly DependencyProperty IconTypePropertyChanged = DependencyProperty.Register("IconType", typeof(PackIconMaterialKind), typeof(OptionButton), new PropertyMetadata(PackIconMaterialKind.Cog));
 
+        private readonly DoubleAnimation ActiveAnimation;
+        private readonly DoubleAnimation InactiveAnimation;
+        private readonly DoubleAnimation OverAnimation;
+
         public OptionButton()
         {
             InitializeComponent();
+            TimeSpan time = TimeSpan.FromSeconds(0.20);
+            OverAnimation = new DoubleAnimation(0.25, time);
+            InactiveAnimation = new DoubleAnimation(0, time);
+            ActiveAnimation = new DoubleAnimation(0.75, time);
         }
 
         private void Option_MouseEnter(object sender, MouseEventArgs e)
         {
             if (!Active)
-                BackColor.Opacity = 0.5;
+                BackColor.BeginAnimation(OpacityProperty, OverAnimation);
         }
 
         private void Option_MouseLeave(object sender, MouseEventArgs e)
         {
-            BackColor.Opacity = Active ? 0.75 : 0.0;
+            if (!Active)
+                BackColor.BeginAnimation(OpacityProperty, InactiveAnimation);
         }
 
         private void Option_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             Active = true;
-            BackColor.Opacity = 1.0;
-        }
-
-        private void Option_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            BackColor.Opacity = 0.5;
         }
     }
 }
