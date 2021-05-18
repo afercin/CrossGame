5a08b34f30f12de9d89cfc9369e5940f67cd9c95
afercin <afercin@gmail.com>
2021-05-18 16:37:48 +0200

    Moved to controlls

--- Cross Game/Cross Game/Controllers/EditComputerParams.xaml.cs
+++ Cross Game/Cross Game/Controllers/EditComputerParams.xaml.cs
@@ -3,7 +3,7 @@ using System.Windows;
 using System.Windows.Controls;
 using Cross_Game.Controllers;
 
-namespace Cross_Game.Windows
+namespace Cross_Game.Controllers
 {
     /// <summary>
     /// Lógica de interacción para EditComputerParams.xaml
